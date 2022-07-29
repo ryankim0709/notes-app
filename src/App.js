41f39d6c3005089172865fd1f0aa4382721cf8de
Ryan Kim <ryankim0709@gmail.com>
2022-07-28 17:03:39 -0700

    Notes APp for recruitment complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [currNote, setCurrNote] = useState("");
+  const [currSubject, setCurrSubject] = useState("");
+  const [notesData, setNotesData] = useState([]);
+
+  function addData() {
+    if (!currNote || !currSubject) {
+      alert("I'm sorry, your notes or subject are empty. Please fill them in");
+      return;
+    } else {
+      var data = notesData;
+      data.push({ notes: currNote, subject: currSubject });
+      setNotesData(data);
+      setCurrNote("");
+      setCurrSubject("");
+      console.log(notesData);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="notesCreator">
+        <form className="noteForm">
+          <textarea
+            className="noteBox"
+            type="text"
+            name="note"
+            placeholder="My notes"
+            value={currNote}
+            onChange={(text) => {
+              setCurrNote(text.target.value);
+            }}
+          />
+        </form>
+        <form>
+          <input
+            className="subjectBox"
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            value={currSubject}
+            onChange={(text) => {
+              setCurrSubject(text.target.value);
+            }}
+          />
+        </form>
+        <button
+          onClick={() => {
+            addData();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Note
+        </button>
+      </div>
+      <div className="notesDisplay">
+        {notesData.map((note, key) => (
+          <div className="noteObj">
+            <div>Subject: {note["subject"]}</div>
+            <div>Note: {note["notes"]}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
