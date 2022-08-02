404dc82d307678b45d508515f1514870bc3d6897
Charger1256A <aditya.ram112005@gmail.com>
2022-08-02 06:22:35 -0700

    fixed css

--- src/App.js
+++ src/App.js
@@ -6,47 +6,39 @@ export default function App() {
   const [currSubject, setCurrSubject] = useState("");
   const [notesData, setNotesData] = useState([]);
 
-  function addData() {
+  const addData = () => {
     if (!currNote || !currSubject) {
       alert("I'm sorry, your notes or subject are empty. Please fill them in");
       return;
-    } else {
-      var data = notesData;
-      data.push({ notes: currNote, subject: currSubject });
-      setNotesData(data);
-      setCurrNote("");
-      setCurrSubject("");
-      console.log(notesData);
     }
+    var data = notesData;
+    data.push({ notes: currNote, subject: currSubject });
+    setNotesData(data);
+    setCurrNote("");
+    setCurrSubject("");
+    console.log(notesData);
   }
 
   return (
     <div className="container">
-      <div className="notesCreator">
-        <form className="noteForm">
+      <div className="inputs">
           <textarea
-            className="noteBox"
+            className="note-box"
             type="text"
-            name="note"
             placeholder="My notes"
             value={currNote}
             onChange={(text) => {
               setCurrNote(text.target.value);
             }}
           />
-        </form>
-        <form>
           <input
-            className="subjectBox"
             type="text"
-            name="subject"
             placeholder="Subject"
             value={currSubject}
             onChange={(text) => {
               setCurrSubject(text.target.value);
             }}
           />
-        </form>
         <button
           onClick={() => {
             addData();
@@ -55,11 +47,11 @@ export default function App() {
           Add Note
         </button>
       </div>
-      <div className="notesDisplay">
+      <div className="container note-container">
         {notesData.map((note, key) => (
-          <div className="noteObj">
-            <div>Subject: {note["subject"]}</div>
-            <div>Note: {note["notes"]}</div>
+          <div className="note-obj">
+            <div className="single-text">Subject: {note["subject"]}</div>
+            <div className="single-text">Note: {note["notes"]}</div>
           </div>
         ))}
       </div>
